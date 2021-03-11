2eb8eb441ed82038394a98c143f90eb7296bbf6c
barkon99 <bartekkonewka@gmail.com>
2021-03-11 11:22:59 +0100

    Adding Lombook to project. Creating Builder for BookResponse class

--- src/main/java/com/konew/backend/model/response/BookResponse.java
+++ src/main/java/com/konew/backend/model/response/BookResponse.java
@@ -1,5 +1,12 @@
 package com.konew.backend.model.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class BookResponse
 {
     private Long id;
@@ -12,97 +19,5 @@ public class BookResponse
     private int amountOfRatings;
     private String imageUrl;
 
-    public BookResponse() {
-    }
-    public BookResponse(Long id, String title, String author)
-    {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
 
-    public BookResponse(Long id, String title, String author, String category, double avg_rate,
-                        int user_rate, String userName, int amountOfRatings, String imageUrl) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.category = category;
-        this.avg_rate = avg_rate;
-        this.user_rate = user_rate;
-        this.userName = userName;
-        this.amountOfRatings = amountOfRatings;
-        this.imageUrl = imageUrl;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public double getAvg_rate() {
-        return avg_rate;
-    }
-
-    public void setAvg_rate(double avg_rate) {
-        this.avg_rate = avg_rate;
-    }
-
-    public int getUser_rate() {
-        return user_rate;
-    }
-
-    public void setUser_rate(int user_rate) {
-        this.user_rate = user_rate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public int getAmountOfRatings() {
-        return amountOfRatings;
-    }
-
-    public void setAmountOfRatings(int amountOfRatings) {
-        this.amountOfRatings = amountOfRatings;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
